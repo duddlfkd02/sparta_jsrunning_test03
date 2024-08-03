032771bfaee5ff22efe60b1629318414fd5e6c45
Hayoung Kim <161779920+duddlfkd02@users.noreply.github.com>
2024-08-03 16:13:20 +0900

    fix: 해설 공부 후 답안 수정

--- 02.js
+++ 02.js
@@ -2,25 +2,26 @@
 
 // 위 1번 문제에서 다루었던 비동기 처리를 `async`, `await` 키워드를 사용하여 코드를 수정해서 작성해주세요.
 
-async function fetchData(url) {
-    try {
-        const data = await getData(url);
-        console.log(data);
-    } catch (err) {
-        console.error(err);
-    }
+function getData(url) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (url === API_URL) {
+        resolve("성공");
+      } else {
+        reject("실패");
+      }
+    }, 3000);
+  });
 }
 
+async function callGetData(url) {
+  try {
+    const result = await getData(url);
+    console.log(result);
+  } catch (error) {
+    consol.log(error);
+  }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
+callGetData(API_URL); //성공한 url
+callGetData(WRONG_URL); //잘못된 url > error 뜸
